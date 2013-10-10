4309f8d615be65ae660861eb207f0b3c7f71860a
Mike Adeleke <madeleke13@gmail.com>
2013-10-09 20:32:41 -0500

    Help with Heroku question

--- app/views/playlists/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @playlist, :name, :created_at, :updated_at
--- db/migrate/20131009044707_create_startups.rb
+++
@@ -1,9 +0,0 @@
-class CreateStartups < ActiveRecord::Migration
-  def change
-    create_table :startups do |t|
-      t.string :description
-
-      t.timestamps
-    end
-  end
-end
