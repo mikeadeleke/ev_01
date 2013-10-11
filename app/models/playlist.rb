975893e80369285399c7f28f00f3068272764d95
Mike Adeleke <madeleke13@gmail.com>
2013-10-11 10:02:05 -0500

    Memberships migration

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,5 @@
 class Playlist < ActiveRecord::Base
 	belongs_to :user
-	has_many :startups, through: :memberships
+	has_many :startups
 end
+
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,6 +1,6 @@
 class Startup < ActiveRecord::Base
 	belongs_to :user
-	has_many :playlists, through: :memberships
+	has_many :playlists
 	def self.search(search)
 	  if search
 	    find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
